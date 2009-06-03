276ea923b84bc52fad93a7c4f70d9d5bf59bf16a
Matthieu Fertré <matthieu.fertre@kerlabs.com>
2009-06-03 13:45:13 +0200

    [BUGFIX] Fix lock releasing when creation of IPC message queue has failed
    Signed-off-by: Matthieu Fertré <matthieu.fertre@kerlabs.com>

--- ipc/msg.c
+++ ipc/msg.c
@@ -245,6 +245,9 @@ static int newque(struct ipc_namespace *ns, struct ipc_params *params)
 	if (is_krg_ipc(&msg_ids(ns))) {
 		retval = kh_ipc_msg_newque(ns, msq) ;
 		if (retval) {
+			/* release locks held by ipc_addid */
+			local_ipc_unlock(&msq->q_perm);
+
 			security_msg_queue_free(msq);
 			ipc_rcu_putref(msq);
 			return retval;
