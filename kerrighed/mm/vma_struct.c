cf1608b89d9a8d06d77b05cffb71701105da5800
Louis Rilling <louis.rilling@kerlabs.com>
2009-06-18 11:38:19 +0200

    [BUGFIX] mm: Make sure the whole fake vma is initialized
    Signed-off-by: Louis Rilling <louis.rilling@kerlabs.com>

--- kerrighed/mm/vma_struct.c
+++ kerrighed/mm/vma_struct.c
@@ -28,16 +28,13 @@ int alloc_fake_vma(struct mm_struct *mm,
 	struct anon_vma *anon_vma;
 	int r = 0;
 
-	vma = kmem_cache_alloc(vm_area_cachep, GFP_ATOMIC);
+	vma = kmem_cache_zalloc(vm_area_cachep, GFP_ATOMIC);
 	if (!vma)
 		return -ENOMEM;
 
 	partial_init_vma (mm, vma);
 	vma->vm_start = start;
 	vma->vm_end = end;
-	vma->vm_file = NULL;
-	vma->vm_ops = NULL;
-	vma->anon_vma = NULL;
 	vma->vm_flags = VM_READ | VM_WRITE | VM_MAYREAD | VM_MAYWRITE |
 		VM_MAYEXEC;
 
