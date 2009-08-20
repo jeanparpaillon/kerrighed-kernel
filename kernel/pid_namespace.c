e2ed3ddb81a84fa5691951901aeb165475a2c3d5
Louis Rilling <louis.rilling@kerlabs.com>
2009-08-20 18:22:12 +0200

    krg_ns/pid: Introduce find_get_krg_pid_ns()
    And export free_pid_ns() by the way, since find_get_krg_pid_ns() must be
    callable by modules, and inline put_pid_ns() references free_pid_ns().
    
    Signed-off-by: Louis Rilling <louis.rilling@kerlabs.com>

--- kernel/pid_namespace.c
+++ kernel/pid_namespace.c
@@ -13,6 +13,10 @@
 #include <linux/syscalls.h>
 #include <linux/err.h>
 #include <linux/acct.h>
+#ifdef CONFIG_KRG_PROC
+#include <linux/module.h>
+#include <kerrighed/namespace.h>
+#endif
 
 #define BITS_PER_PAGE		(PAGE_SIZE*8)
 
@@ -162,6 +166,9 @@ void free_pid_ns(struct kref *kref)
 	if (parent != NULL)
 		put_pid_ns(parent);
 }
+#ifdef CONFIG_KRG_PROC
+EXPORT_SYMBOL(free_pid_ns);
+#endif
 
 void zap_pid_ns_processes(struct pid_namespace *pid_ns)
 {
@@ -210,6 +217,17 @@ void zap_pid_ns_processes(struct pid_namespace *pid_ns)
 	acct_exit_ns(pid_ns);
 	return;
 }
+
+#ifdef CONFIG_KRG_PROC
+struct pid_namespace *find_get_krg_pid_ns(void)
+{
+	struct krg_namespace *krg_ns = find_get_krg_ns();
+	struct pid_namespace *ns = get_pid_ns(krg_ns->root_pid_ns);
+	put_krg_ns(krg_ns);
+	return ns;
+}
+EXPORT_SYMBOL(find_get_krg_pid_ns);
+#endif
 
 static __init int pid_namespaces_init(void)
 {
