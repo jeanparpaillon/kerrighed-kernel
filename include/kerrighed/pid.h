b60462bae5409c257f84c2b4bb6728d8ed2dd6a4
Louis Rilling <louis.rilling@kerlabs.com>
2009-08-20 16:09:59 +0200

    [JANITOR] krg_ns/pid: Kill current_pid_mask
    And:
    - kill (now unused) derived functions/macros as well;
    - rename global_pid_init() which does nothing with PIDs anymore.
    
    Signed-off-by: Louis Rilling <louis.rilling@kerlabs.com>

--- include/kerrighed/pid.h
+++ include/kerrighed/pid.h
@@ -18,31 +18,8 @@
 #define PID_NODE_SHIFT (NR_BITS_PID_MAX_LIMIT + 1)
 #define INTERNAL_PID_MASK (PID_MAX_LIMIT - 1)
 
-extern pid_t current_pid_mask;
-
-/* No type checking, no current_pid_mask checking */
 #define __MAKE_KERRIGHED_PID_FOR_NODE(pid, node) \
 	(((node) << PID_NODE_SHIFT)|GLOBAL_PID_MASK|((pid) & INTERNAL_PID_MASK))
-
-/* Same with checking */
-static inline pid_t make_kerrighed_pid_for_node(pid_t pid,
-						kerrighed_node_t node)
-{
-	pid_t ret;
-
-	if (likely(current_pid_mask))
-		ret = __MAKE_KERRIGHED_PID_FOR_NODE(pid, node);
-	else
-		ret = pid;
-
-	return ret;
-
-}
-
-#define MAKE_KERRIGHED_PID_FOR_NODE(pid,node) \
-	make_kerrighed_pid_for_node(pid, node)
-#define MAKE_KERRIGHED_PID(pid) \
-	MAKE_KERRIGHED_PID_FOR_NODE(pid,kerrighed_node_id)
 
 /** extract the original linux kernel pid of a Kerrighed PID */
 #define SHORT_PID(pid) ((pid) & INTERNAL_PID_MASK)
