0ccc8df0c2938d40895cdc2e86ce69f141c2cd00
Louis Rilling <louis.rilling@kerlabs.com>
2009-08-28 19:22:50 +0200

    krg_ns/pid/sighand: Convert to *_knr()
    Signed-off-by: Louis Rilling <louis.rilling@kerlabs.com>

--- kerrighed/epm/sighand.c
+++ kerrighed/epm/sighand.c
@@ -282,7 +282,8 @@ static void __krg_sighand_alloc(struct task_struct *task,
 	 * is being allocated, but we only need to know whether it is NULL or
 	 * not, which will be the same after copy_mm.
 	 */
-	if (!(task->pid & GLOBAL_PID_MASK) || (task->flags & PF_KTHREAD)) {
+	if (!(task_pid_knr(task) & GLOBAL_PID_MASK)
+	    || (task->flags & PF_KTHREAD)) {
 		BUG_ON(krg_current);
 		sig->krg_objid = 0;
 		sig->kddm_obj = NULL;
