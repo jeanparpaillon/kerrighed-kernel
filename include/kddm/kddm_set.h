b3a5776b63ec50a7596c50b56580c74394388452
Renaud Lottiaux <Renaud.Lottiaux@kerlabs.com>
2009-10-28 17:54:52 +0100

    [MINOR] Cleanup KDDM set ids list.

--- include/kddm/kddm_set.h
+++ include/kddm/kddm_set.h
@@ -67,50 +67,36 @@ enum
 enum
   {
     KDDM_SET_UNUSED,                  //  0
-    KDFS_FILE_KDDM__ID,	              //  1
-    KDFS_INODE_KDDM_ID,               //  2
-    KDFS_DENTRY_KDDM_ID,              //  3
-    TASK_KDDM_ID,                     //  4
-    SIGNAL_STRUCT_KDDM_ID,            //  5
-    SIGHAND_STRUCT_KDDM_ID,           //  6
-    STATIC_NODE_INFO_KDDM_ID,         //  7
-    STATIC_CPU_INFO_KDDM_ID,          //  8
-    DYNAMIC_NODE_INFO_KDDM_ID,        //  9
-    DYNAMIC_CPU_INFO_KDDM_ID,         // 10
-    MEMORY_NODE_INFO_KDDM_ID,         // 11
-    STREAM_KDDM_ID,                   // 12
-    SOCKET_KDDM_ID,                   // 13
-    APP_KDDM_ID,                      // 14
-    FUTEX_KDDM_ID,                    // 15
-    SHMID_KDDM_ID,                    // 16
-    SHMKEY_KDDM_ID,                   // 17
-    SHMMAP_KDDM_ID,                   // 18
-    SEMARRAY_KDDM_ID,                 // 19
-    SEMKEY_KDDM_ID,                   // 20
-    SEMMAP_KDDM_ID,                   // 21
-    SEMUNDO_KDDM_ID,                  // 22
-    MSG_KDDM_ID,                      // 23
-    MSGKEY_KDDM_ID,                   // 24
-    MSGMAP_KDDM_ID,                   // 25
-    MSGMASTER_KDDM_ID,                // 26
-    DSTREAM_KDDM_ID,                  // 27
-    DSTREAM_SOCKET_BASE,              // 28
-    DSTREAM_SOCKET_MAX = DSTREAM_SOCKET_BASE + AF_MAX, //  + 32 = 60
-    DSTREAM_PIPE_KDDM_ID,             // 61
-    PID_KDDM_ID,                      // 62
-    CHILDREN_KDDM_ID,                 // 63
-    DVFS_FILE_STRUCT_KDDM_ID,         // 64
-    GLOBAL_LOCK_KDDM_SET_ID,	      // 65
-    GLOBAL_CONFIG_KDDM_SET_ID,        // 66
-    KDDM_TEST4_DIST,                  // 67
-    KDDM_TEST4_LOC,                   // 68
-    KDDM_TEST4096,                    // 69
-    MM_STRUCT_KDDM_ID,                // 70
+    TASK_KDDM_ID,                     //  1
+    SIGNAL_STRUCT_KDDM_ID,            //  2
+    SIGHAND_STRUCT_KDDM_ID,           //  3
+    STATIC_NODE_INFO_KDDM_ID,         //  4
+    STATIC_CPU_INFO_KDDM_ID,          //  5
+    DYNAMIC_NODE_INFO_KDDM_ID,        //  6
+    DYNAMIC_CPU_INFO_KDDM_ID,         //  7
+    APP_KDDM_ID,                      //  8
+    SHMID_KDDM_ID,                    //  9
+    SHMKEY_KDDM_ID,                   // 10
+    SHMMAP_KDDM_ID,                   // 11
+    SEMARRAY_KDDM_ID,                 // 12
+    SEMKEY_KDDM_ID,                   // 13
+    SEMMAP_KDDM_ID,                   // 14
+    SEMUNDO_KDDM_ID,                  // 15
+    MSG_KDDM_ID,                      // 16
+    MSGKEY_KDDM_ID,                   // 17
+    MSGMAP_KDDM_ID,                   // 18
+    MSGMASTER_KDDM_ID,                // 19
+    PID_KDDM_ID,                      // 20
+    CHILDREN_KDDM_ID,                 // 21
+    DVFS_FILE_STRUCT_KDDM_ID,         // 22
+    GLOBAL_LOCK_KDDM_SET_ID,	      // 23
+    GLOBAL_CONFIG_KDDM_SET_ID,        // 24
+    KDDM_TEST4_DIST,                  // 25
+    KDDM_TEST4_LOC,                   // 26
+    KDDM_TEST4096,                    // 27
+    MM_STRUCT_KDDM_ID,                // 28
     MIN_KDDM_ID,           /* MUST always be the last one */
   };
-
-#define DSTREAM_UNIX_KDDM_ID (DSTREAM_SOCKET_BASE+AF_UNIX)
-#define DSTREAM_INET_KDDM_ID (DSTREAM_SOCKET_BASE+AF_INET)
 
 
 
