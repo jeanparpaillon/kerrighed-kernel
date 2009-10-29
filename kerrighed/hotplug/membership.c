92c19f40860579456af8c5297b97e6d8a91ad924
Louis Rilling <louis.rilling@kerlabs.com>
2009-10-29 17:50:47 +0100

    hotplug/remove: Introduce membership_online_hold()/release()
    In order to prevent some Kerrighed services, like process migration, to inject
    objects to nodes being removed, or to block trying to call a disabled service of
    a removing node, this commit introduces membership_online_hold()/release() to
    temporarily prevent remove operations from removing nodes from the online map,
    which in turn ensures that nodes being removed are kept active.
    
    Signed-off-by: Louis Rilling <louis.rilling@kerlabs.com>

--- kerrighed/hotplug/membership.c
+++ kerrighed/hotplug/membership.c
@@ -1,8 +1,23 @@
 #include <linux/notifier.h>
+#include <linux/rwsem.h>
+#include <linux/cluster_barrier.h>
 #include <kerrighed/hotplug.h>
 #include <kerrighed/krgnodemask.h>
 #include <kerrighed/sys/types.h>
 #include <kerrighed/krginit.h>
+
+static DECLARE_RWSEM(online_sem);
+static struct cluster_barrier *online_barrier;
+
+void membership_online_hold(void)
+{
+	down_read_non_owner(&online_sem);
+}
+
+void membership_online_release(void)
+{
+	up_read_non_owner(&online_sem);
+}
 
 static void membership_online_add(krgnodemask_t *vector)
 {
@@ -12,20 +27,42 @@ static void membership_online_add(krgnodemask_t *vector)
 	kerrighed_nb_nodes += krgnodes_weight(*vector);
 }
 
-static void membership_online_remove(krgnodemask_t *vector)
+static int membership_online_remove(krgnodemask_t *vector)
 {
+	krgnodemask_t nodes;
+	kerrighed_node_t master;
+	int err;
+
 	BUG_ON(!krgnodes_subset(*vector, krgnode_online_map));
+
+	down_write(&online_sem);
+
+	krgnodes_copy(nodes, krgnode_online_map);
+	master = first_krgnode(nodes);
+
+	err = cluster_barrier(online_barrier, &nodes, master);
+	if (err)
+		goto unlock;
 
 	krgnodes_andnot(krgnode_online_map, krgnode_online_map, *vector);
 	kerrighed_nb_nodes -= krgnodes_weight(*vector);
+
+	err = cluster_barrier(online_barrier, &nodes, master);
+
+unlock:
+	up_write(&online_sem);
+
+	return err;
 }
 
 static void membership_online_clear(krgnodemask_t *vector)
 {
 	BUG_ON(!krgnode_isset(kerrighed_node_id, *vector));
 
+	down_write(&online_sem);
 	krgnodes_clear(krgnode_online_map);
 	kerrighed_nb_nodes = 0;
+	up_write(&online_sem);
 }
 
 static
@@ -71,6 +108,10 @@ int membership_present_notification(struct notifier_block *nb,
 
 int hotplug_membership_init(void)
 {
+	online_barrier = alloc_cluster_barrier(ONLINE_HOTPLUG_BARRIER);
+	if (IS_ERR(online_barrier))
+		panic("kerrighed: Couldn't alloc online_barrier!\n");
+
 	register_hotplug_notifier(membership_present_notification,
 				  HOTPLUG_PRIO_MEMBERSHIP_PRESENT);
 	register_hotplug_notifier(membership_online_notification,
