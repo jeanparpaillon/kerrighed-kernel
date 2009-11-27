b867870c019b859e64e41e6af83b79cc6199d88e
Matthieu Fertré <matthieu.fertre@kerlabs.com>
2009-11-27 11:01:55 +0100

    Factorize code to send a FAFFED file descriptor over RPC.
    This is used to restart an application with a new tty, and to
    checkpoint message queue hosted on a different node.

--- kerrighed/epm/application/app_terminal.c
+++ kerrighed/epm/application/app_terminal.c
@@ -111,92 +111,25 @@ struct file *app_get_restart_terminal(struct app_struct *app)
 
 int send_terminal_desc(struct rpc_desc *desc, struct file *tty)
 {
-	void *fdesc;
-	int fdesc_size;
 	int r;
 
-	if (!tty->f_objid) {
-		r = create_kddm_file_object(tty);
-		if (r)
-			goto error;
-        }
+	r = send_faf_file_desc(desc, tty);
 
-	r = setup_faf_file_if_needed(tty);
-	if (r == -EALREADY)
-		r = 0;
-
-	if (!(tty->f_flags & (O_FAF_SRV | O_FAF_CLT)))
-		r = -EINVAL;
-
-	if (r)
-		goto error;
-
-	r = get_faf_file_krg_desc(tty, &fdesc, &fdesc_size);
-	if (r)
-		goto error;
-
-	r = rpc_pack_type(desc, tty->f_objid);
-	if (r)
-		goto err_free_desc;
-
-	r = rpc_pack_type(desc, fdesc_size);
-	if (r)
-		goto err_free_desc;
-
-	r = rpc_pack(desc, 0, fdesc, fdesc_size);
-
-err_free_desc:
-	kfree(fdesc);
-
-error:
 	return r;
 }
 
 int rcv_terminal_desc(struct rpc_desc *desc, struct app_struct *app)
 {
-	int r;
-	void *fdesc;
-	int fdesc_size;
-	long fobjid;
-	struct dvfs_file_struct *dvfs_file = NULL;
+	int r = 0;
 	struct file *file;
-	int first_import = 0;
 
-	r = rpc_unpack_type(desc, fobjid);
-	if (r)
-		goto error;
-
-	r = rpc_unpack_type(desc, fdesc_size);
-	if (r)
-		goto error;
-
-	fdesc = kmalloc(GFP_KERNEL, fdesc_size);
-	if (!fdesc) {
-		r = -ENOMEM;
+	file = rcv_faf_file_desc(desc);
+	if (IS_ERR(file)) {
+		r = PTR_ERR(file);
 		goto error;
 	}
-
-	r = rpc_unpack(desc, 0, fdesc, fdesc_size);
-	if (r)
-		goto err_free_desc;
-
-	/* Check if the file struct is already present */
-	file = begin_import_dvfs_file(fobjid, &dvfs_file);
-
-	if (!file) {
-		file = create_faf_file_from_krg_desc(current, fdesc);
-		first_import = 1;
-	}
-
-	r = end_import_dvfs_file(fobjid, dvfs_file, file, first_import);
-	if (r)
-		goto err_free_desc;
 
 	app_set_restart_terminal(app, file);
-
-err_free_desc:
-	kfree(fdesc);
-
 error:
 	return r;
 }
