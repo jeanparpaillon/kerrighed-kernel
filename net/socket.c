73778b93a6c70b0322be12677b75dd5270c35588
Matthieu Fertré <matthieu.fertre@kerlabs.com>
2010-07-02 16:17:06 +0200

    Forward socket only if USE_WORLD_VISIBLE_KERSTREAMS capability is set

--- net/socket.c
+++ net/socket.c
@@ -1295,7 +1295,8 @@ SYSCALL_DEFINE3(socket, int, family, int, type, int, protocol)
 #ifdef CONFIG_KRG_FAF
 	if (current->nsproxy->krg_ns && krgnode_online(kerrighed_node_id)) {
 		int master_node_id = current->master_node;
-		if (kerrighed_node_id != master_node_id
+		if (can_use_krg_cap(current, CAP_USE_WORLD_VISIBLE_KERSTREAMS)
+		    && kerrighed_node_id != master_node_id
 		    && krgnode_online(master_node_id)) {
 			retval = krg_faf_socket(master_node_id, family, type, protocol);
 			return retval;
