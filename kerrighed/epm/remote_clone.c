ee74f3ae79fa92c5355d56551f34c9651ddb0aca
Louis Rilling <louis.rilling@kerlabs.com>
2010-09-29 17:54:52 +0200

    remote pthread_create() debug

--- kerrighed/epm/remote_clone.c
+++ kerrighed/epm/remote_clone.c
@@ -61,7 +61,9 @@ int krg_do_fork(unsigned long clone_flags,
 	       CLONE_VFORK | CLONE_SYSVSEM | CLONE_UNTRACED))
 	    || trace) {
 		/* Authorize LinuxThreads' pthread_create() */
-		if ((clone_flags & ~CSIGNAL) != (CLONE_VM|CLONE_FS|CLONE_FILES|CLONE_SIGHAND))
+		if ((clone_flags & ~CSIGNAL) == (CLONE_VM|CLONE_FS|CLONE_FILES|CLONE_SIGHAND))
+			printk("kerrighed %d(%s): Remote pthread_create() attempted\n", task_pid_knr(current), current->comm);
+		else
 			/* Unsupported clone flags are requested. Abort */
 			goto out;
 	}
